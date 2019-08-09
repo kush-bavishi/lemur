97e356a9e91cf50ecf1e6fb3b183f28f07455671
Kush Bavishi <kbavishi@netflix.com>
2019-08-09 10:23:29 -0700

    Revert "Resolve merge conflict"
    This reverts commit f474a758c997e64719379ad810946917fb50992f.

--- lemur/plugins/lemur_vault_dest/plugin.py
+++ lemur/plugins/lemur_vault_dest/plugin.py
@@ -67,15 +67,15 @@ class VaultSourcePlugin(SourcePlugin):
             'name': 'vaultPath',
             'type': 'str',
             'required': True,
-            'validation': '^([a-zA-Z0-9._-]+/?)+$',
+            'validation': "^([a-zA-Z0-9._-]+/?)+$",
             'helpMessage': 'Must be a valid Vault secrets path',
         },
         {
-            'name': 'objectName',
-            'type': 'str',
-            'required': True,
-            'validation': '[0-9a-zA-Z.:_-]+',
-            'helpMessage': 'Object Name to search',
+            "name": "objectName",
+            "type": "str",
+            "required": True,
+            "validation": "[0-9a-zA-Z.:_-]+",
+            "helpMessage": "Object Name to search",
         },
     ]
 
